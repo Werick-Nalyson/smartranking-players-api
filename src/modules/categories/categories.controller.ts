da2ecc0f07052df18a6b7fb45cfdceb563c5859f
Werick Nalyson <wericknalyson@gmail.com>
2023-02-01 22:04:23 -0300

    feat create categorie

--- src/modules/categories/categories.controller.ts
+++ src/modules/categories/categories.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { CreateCategorieDto } from './dto/createCategorie.dto';
 
-@Controller('api/v1/categorias')
-export class CategoriesController {}
+@Controller('api/v1/categories')
+export class CategoriesController {
+  constructor(private readonly categoriesService: CategoriesService) {}
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  create(@Body() createCategorieDto: CreateCategorieDto) {
+    return this.categoriesService.create(createCategorieDto);
+  }
+}
--- src/modules/categories/categories.service.ts
+++ src/modules/categories/categories.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { BadRequestException } from '@nestjs/common/exceptions';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateCategorieDto } from './dto/createCategorie.dto';
+import { Categorie } from './interface/categorie.interface';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(
+    @InjectModel('Categorie') private readonly categorieModel: Model<Categorie>,
+  ) {}
+
+  async create(createCategorieDto: CreateCategorieDto): Promise<Categorie> {
+    const { categorie, description, events } = createCategorieDto;
+
+    const categoriaEncotrada = await this.categorieModel
+      .findOne({ categorie })
+      .exec();
+
+    if (categoriaEncotrada) {
+      throw new BadRequestException(`Categorie ${categorie} already exists!`);
+    }
+
+    const categoriaCriada = new this.categorieModel({
+      categorie,
+      description,
+      events,
+    });
+
+    return await categoriaCriada.save();
+  }
+}
